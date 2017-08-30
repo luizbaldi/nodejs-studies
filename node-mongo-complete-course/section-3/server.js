235714a7ec7576804ad84f9bc971700d71bf972b
Luiz Baldi <baldilp@gmail.com>
2017-08-30 15:37:24 -0300

    initial commit with sections 2 and 3 done

---
+++ node-mongo-complete-course/section-3/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+    res.render('home/index
+    ');
+});
+
+server.get('/formulario_inclusao_noticia', (req, res) => {
+    res.render('admin/form_add_noticia');
+});
+
+server.get('/noticias', (req, res) => {
+    res.render('noticias/noticias');
+});
+
+server.listen(9090, () => {
+    console.log('Servidor iniciado!');
+});
